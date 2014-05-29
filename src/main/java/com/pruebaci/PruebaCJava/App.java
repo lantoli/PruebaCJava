97831f905c4798d573bf992e18d7a0535677995e
Leo Antoli <leo.antoli@gmail.com>
2014-05-29 16:53:13 +0200

    initial commit

---
+++ src/main/java/com/pruebaci/PruebaCJava/App.java
@@ -0,0 +1,13 @@
+package com.pruebaci.PruebaCJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
